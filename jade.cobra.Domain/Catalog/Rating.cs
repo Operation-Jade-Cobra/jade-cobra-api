f30db9784b2ef20c91a47a568daba26cc29aac1e
Alex Leyland <alexleyland@gmail.com>
2025-03-25 09:29:32 -0400

    Catalog Completion

--- jade.cobra.Domain/Catalog/Rating.cs
+++ jade.cobra.Domain/Catalog/Rating.cs
@@ -3,8 +3,8 @@ namespace jade.cobra.Domain.Catalog;
 public class Rating
 {
     public int Stars {get;set;}
-    public string UserName {get;set;}
-    public string Review {get;set;}
+    public string? UserName {get;set;}
+    public string? Review {get;set;}
 
 public Rating (int stars, string userName, string review)
 {
