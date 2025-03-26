3dfd1ad18119b0c11b830bc4af42f636ae1d5d6b
Deborah Lee <deborah8dublin@hotmail.com>
2025-03-25 22:28:02 -0400

    updating Post "rating" (PP.26)

--- jade-cobra-api-ef-core/jade.cobra.Api/Controllers/CatalogController.cs
+++ jade-cobra-api-ef-core/jade.cobra.Api/Controllers/CatalogController.cs
@@ -49,9 +49,12 @@ namespace jade.cobra.Api.Controllers
         [HttpPost("{id:int}/ratings")]
         public IActionResult PostRating(int id, [FromBody] Rating rating)
         {
-            var item = new Item("Shirt", "Ohio State shirt", "Nike", 29.99m);
-            item.Id = id;
+            var item = _db.items.Find(id);
+            if (item == null) {
+                return NotFound();
+            }
             item.AddRating(rating);
+            _db.SaveChanged();
 
             return Ok(item);
         }
