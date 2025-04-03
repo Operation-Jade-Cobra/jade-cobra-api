8740d99b3f61e4bbe7ec5d986a85514b742f737b
Deborah Lee <deborah8dublin@hotmail.com>
2025-04-03 14:53:22 -0400

    creating test folder

---
+++ tests/jade.cobra.Domain.Tests/RatingTests.cs
@@ -0,0 +1,16 @@
+﻿using jade.cobra.Domain;
+using jade.cobra.Domain.Catalog;
+namespace jade.cobra.Domain.Tests;
+
+[TestClass]
+public class RatingTests
+{
+    [TestMethod]
+    public void Can_Create_New_Rating()
+    {
+        var rating = new Rating(1, "Mike", "Great fit!");
+        Assert.AreEqual(1, rating.Stars);
+        Assert.AreEqual("Mike", rating.UserName);
+        Assert.AreEqual("Great fit!", rating.Review);
+    }
+}
